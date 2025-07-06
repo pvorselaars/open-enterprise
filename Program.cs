3ae1605e9a0a640b4523778a7317589fda403591
Perry Vorselaars <p.vorselaars@gmail.com>
2025-07-06 11:07:30 +0200

    Implement OIDC authentication

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ using OpenEnterprise.UI;
 using OpenEnterprise.Ontology;
 using OpenEnterprise.Models;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,10 +21,48 @@ builder.WebHost.ConfigureKestrel(options =>
 
 });
 
+builder.Services.AddAuthentication(options => {
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = builder.Configuration["Authentication:OIDC:Authority"];
+                    options.ClientId = builder.Configuration["Authentication:OIDC:ClientId"];
+                    options.ClientSecret = builder.Configuration["Authentication:OIDC:ClientSecret"];
+                    options.CallbackPath = builder.Configuration["Authentication:OIDC:CallbackPath"];
+
+                    options.ResponseType = OpenIdConnectResponseType.Code;
+                    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+                    options.SaveTokens = true;
+
+                    options.ClaimActions.MapJsonKey("name", "name");
+                    options.TokenValidationParameters.NameClaimType = "name";
+
+                    var scopes = builder.Configuration.GetSection("Authentication:OIDC:Scope").Get<string[]>();
+                    if (scopes != null)
+                    {
+                        foreach (var scope in scopes)
+                        {
+                            options.Scope.Add(scope);
+                        }
+                    }
+
+                });
+
+builder.Services.AddAuthorizationBuilder()
+    .SetFallbackPolicy(new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build());
+
 builder.Services.AddSingleton<HashSet<Role>>([new("Alice"), new("Bob"), new("John")]);
 builder.Services.AddSingleton<HashSet<ProductionFact>>([]);
 builder.Services.AddSingleton<HashSet<CommunicationFact>>([]);
 builder.Services.AddSingleton<ProcessModel>();
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -27,9 +70,12 @@ StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configurat
 
 var app = builder.Build();
 
+app.UseExceptionHandler("/error");
 app.UseHsts();
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
